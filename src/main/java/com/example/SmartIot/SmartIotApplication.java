1458733eaa00c2e562fe692fd0c87c97f29f8794
Mikhail <ting0919764920@gmail.com>
2024-06-28 00:19:45 +0800

    first build

---
+++ src/main/java/com/example/SmartIot/SmartIotApplication.java
@@ -0,0 +1,13 @@
+package com.example.SmartIot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SmartIotApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SmartIotApplication.class, args);
+	}
+
+}
